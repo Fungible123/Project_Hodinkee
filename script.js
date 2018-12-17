6ab3e52e28714209bc653a1d6a6338e477d4cd4d
Donny Layug <donn.layug@yahoo.com>
2018-12-17 18:41:15 +0800

    finalized the javascript effects on the sections and brands menu

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-function myFunction() {
-   let dropDown = document.querySelector("#sections a");
+// this is the function for section menu
 
-   dropDown.addEventListener("click", function (e) {
+function myFunction() {
+   let dropDown = document.querySelector("#sections a"); //selecting the sections link
+
+   dropDown.addEventListener("click", function (e) { //adding an event listener to the sections link
         let panel = document.querySelector("#myDropdown");
         let panel2 = document.querySelector("#myDropdown2");
-
         let originalOpacity = 1;
+
+        // checking if opacity is set to 1 or 0 then applying necessary opacity style to display dropdown
         if (panel.style.opacity == originalOpacity) {
             panel.style.opacity = 0;
+            
         } else {
             panel.style.opacity = originalOpacity;
             panel2.style.opacity = 0;
         }
-        
-    e.preventDefault();
+
+        // close sections menu when clicked outside 
+        window.onclick = function(event){  
+            if (!event.target.contains(dropDown)) {
+              panel.style.opacity = 0;
+            }
+        }
+
+        // close sections menu when page is scrolled
+
+        window.onscroll = function(){
+            let currentScrollPos = window.pageYOffset;
+            if (currentScrollPos) {
+                panel.style.opacity = 0;
+            }
+        }
+
+        e.preventDefault();
     });
+    
 }
 
 myFunction();
+
+// this is the function for brands menu
 
 function mySecondFunction() {
     let dropDown = document.querySelector("#brands a");
@@ -25,15 +48,32 @@ function mySecondFunction() {
     dropDown.addEventListener("click", function (e) {
         let panel2 = document.querySelector("#myDropdown2");
         let panel1 = document.querySelector("#myDropdown");
-
         let originalOpacity2 = 1;
+
         if (panel2.style.opacity == originalOpacity2) {
             panel2.style.opacity = 0;
         } else {
             panel2.style.opacity = originalOpacity2;
             panel1.style.opacity = 0;
         }  
-    e.preventDefault();
+
+        // close brands menu when clicked outside 
+        window.onclick = function(event){  
+            if (!event.target.contains(dropDown)) {
+              panel2.style.opacity = 0;
+            }
+        }
+
+        // close brands menu when page is scrolled
+
+        window.onscroll = function(){
+            let currentScrollPos = window.pageYOffset;
+            if (currentScrollPos) {
+                panel2.style.opacity = 0;
+            }
+        }
+
+        e.preventDefault();
     });
 }
 
