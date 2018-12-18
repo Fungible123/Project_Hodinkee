fca155e60c87d94eca246e744d2ef3325a5a9c6c
Donny Layug <donn.layug@yahoo.com>
2018-12-18 19:39:39 +0800

    fixed issues with the sections and brands menu and completed javascript effect on search menu

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ function myFunction() {
    let dropDown = document.querySelector("#sections a"); //selecting the sections link
 
    dropDown.addEventListener("click", function (e) { //adding an event listener to the sections link
-        let panel = document.querySelector("#myDropdown");
+        let panel1 = document.querySelector("#myDropdown");
         let panel2 = document.querySelector("#myDropdown2");
         let originalOpacity = 1;
 
         // checking if opacity is set to 1 or 0 then applying necessary opacity style to display dropdown
-        if (panel.style.opacity == originalOpacity) {
-            panel.style.opacity = 0;
-            
+        if (panel1.style.opacity == originalOpacity) {
+            panel1.style.opacity = 0;
+            panel1.style.visibility = "hidden";
         } else {
-            panel.style.opacity = originalOpacity;
+            panel1.style.opacity = originalOpacity;
             panel2.style.opacity = 0;
+            panel1.style.visibility = "visible";
         }
 
         // close sections menu when clicked outside 
         window.onclick = function(event){  
             if (!event.target.contains(dropDown)) {
-              panel.style.opacity = 0;
+              panel1.style.opacity = 0;
             }
         }
 
@@ -29,7 +30,7 @@ function myFunction() {
         window.onscroll = function(){
             let currentScrollPos = window.pageYOffset;
             if (currentScrollPos) {
-                panel.style.opacity = 0;
+                panel1.style.opacity = 0;
             }
         }
 
@@ -46,21 +47,24 @@ function mySecondFunction() {
     let dropDown = document.querySelector("#brands a");
 
     dropDown.addEventListener("click", function (e) {
-        let panel2 = document.querySelector("#myDropdown2");
-        let panel1 = document.querySelector("#myDropdown");
+        
+        let panel3 = document.querySelector("#myDropdown2");
+        let panel4 = document.querySelector("#myDropdown");
         let originalOpacity2 = 1;
 
-        if (panel2.style.opacity == originalOpacity2) {
-            panel2.style.opacity = 0;
+        if (panel3.style.opacity == originalOpacity2) {
+            panel3.style.opacity = 0;
+            panel3.style.visibility = "hidden";
         } else {
-            panel2.style.opacity = originalOpacity2;
-            panel1.style.opacity = 0;
+            panel3.style.opacity = originalOpacity2;
+            panel4.style.opacity = 0;
+            panel3.style.visibility = "visible";
         }  
 
         // close brands menu when clicked outside 
         window.onclick = function(event){  
             if (!event.target.contains(dropDown)) {
-              panel2.style.opacity = 0;
+            panel3.style.opacity = 0;
             }
         }
 
@@ -69,7 +73,7 @@ function mySecondFunction() {
         window.onscroll = function(){
             let currentScrollPos = window.pageYOffset;
             if (currentScrollPos) {
-                panel2.style.opacity = 0;
+                panel3.style.opacity = 0;
             }
         }
 
@@ -78,3 +82,40 @@ function mySecondFunction() {
 }
 
 mySecondFunction();
+
+// function for search menu
+
+function myThirdFunction() {
+    let openSearch = document.querySelector("#search-icon a");
+
+    openSearch.addEventListener("click", function () {
+        let searchBox = document.querySelector(".search-input-container");
+        let quickLinks = document.querySelector(".search-quicklinks");
+        let normalOpacity = 1;
+
+        if (searchBox.style.opacity == normalOpacity) {
+            searchBox.style.opacity = 0;
+            searchBox.style.visibility = "hidden";
+            searchBox.style.width = 0;
+            quickLinks.style.display = "none";
+        } else {
+            searchBox.style.opacity = normalOpacity;
+            searchBox.style.visibility = "visible";
+            searchBox.style.width = "600px";
+            quickLinks.style.display = "block";
+        }  
+
+        //close search bar when close search icon is clicked 
+        
+        document.querySelector(".close-search").addEventListener("click", function(){
+            searchBox.style.opacity = 0;
+            searchBox.style.visibility = "hidden";
+            searchBox.style.width = 0;
+            quickLinks.style.display = "none";
+        });
+
+        e.preventDefault();
+    });
+}
+
+myThirdFunction();
